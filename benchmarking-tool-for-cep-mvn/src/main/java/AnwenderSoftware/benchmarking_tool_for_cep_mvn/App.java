371e5ed9c973c39b1a126751438493f9d227229a
danielkruegercs <daniel.krueger.cs@gmail.com>
2017-09-18 17:57:40 +0200

    Deleted esper folder. Deleted cep project and replaced it with cep project with maven support.

---
+++ benchmarking-tool-for-cep-mvn/src/main/java/AnwenderSoftware/benchmarking_tool_for_cep_mvn/App.java
@@ -0,0 +1,13 @@
+package AnwenderSoftware.benchmarking_tool_for_cep_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
